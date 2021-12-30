af184f64d322cee7dd860d94590a81bc760150f3
ArnoldLeo <arnoldleoar@gmail.com>
2021-12-30 15:19:12 +0530

    updated

--- c2p-3.js
+++ c2p-3.js
@@ -1,33 +1,36 @@
-class Vehicle extends React.Component{
-    state={
-        vehicleList:''
+class Vehicle extends React.Component {
+    state = {
+        vehicleList: []
     }
-    render(){
-        let {vehicleList}=this.state
+    render() {
+        let { vehicleList } = this.state
         return <div>
             <h1>Vehicle Store</h1>
+
+            {/* {list} */}
             
-                {/* {list} */}
-                {vehicleList.map((val,idx)=>{
-            return <li key={idx}>{val}</li>
-        })}
-            
+            {
+                vehicleList.map((val, idx) => {
+                    return <li key={idx}>{val}</li>
+                })
+            }
+
             <List reply={this.getList.bind(this)} />
         </div>
     }
-    getList(data){
+    getList(data) {
         this.setState({
-            vehicleList:data
-        },()=>{console.log(this.state.vehicleList)})
-        console.log('data:',data);
+            vehicleList: data
+        }, () => { console.log(this.state.vehicleList) })
+        console.log('data:', data);
         // console.log('List',this.state.vehicleList);
     }
 }
-function List(props){
-    let bikes=['Apache','Pulsar','Royal Enfield','Avenger','Splendor']
+function List(props) {
+    let bikes = ['Apache', 'Pulsar', 'Royal Enfield', 'Avenger', 'Splendor']
     return <div>
-        <button onClick={()=>{props.reply(bikes)}}>Click Here</button>
+        <button onClick={() => { props.reply(bikes) }}>Click Here</button>
     </div>
 }
 
-ReactDOM.render(<Vehicle />,document.getElementById('container'))
+ReactDOM.render(<Vehicle />, document.getElementById('container'))
